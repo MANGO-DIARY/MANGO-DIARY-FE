e2da5614ac9eab6404a33608fd8dc9b38098ac6d
minjeoong <orange_mj@naver.com>
2024-07-29 02:51:42 +0900

    ✨ Feat: 닉네임 재설정페이지 퍼블리싱

--- src/pages/nickNameReset/styles.js
+++ src/pages/nickNameReset/styles.js
@@ -2,14 +2,14 @@ import styled from '@emotion/styled';
 import { fontGenerator } from '../../styles/styles';
 import { Colors } from '../../styles/colors';
 
-export const PasswordResetWrap = styled.main`
+export const NickNameResetWrap = styled.main`
   width: 100%;
 
   & > .top {
     display: flex;
     flex-direction: column;
     gap: 15px;
-    padding: 9vh 30px;
+    padding: 11vh 30px;
     text-align: center;
     & > img {
       height: 104px;
