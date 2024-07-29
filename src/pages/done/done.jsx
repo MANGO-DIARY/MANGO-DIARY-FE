fac839cf345899da267aae191a90a91c0ec14524
minjeoong <orange_mj@naver.com>
2024-07-30 04:10:18 +0900

    ✨ Feat: 회원가입 완료 페이지 퍼블리싱

--- src/pages/done/done.jsx
+++ src/pages/done/done.jsx
@@ -14,16 +14,16 @@ function Done() {
       <Header title="이메일로 로그인" iconSrc={Images.left} />
       <div className="top">
         <img src={Images.happy} alt="기쁨이 이미지" />
+        <div className="title">반가워요!</div>
         <div className="comment">
-          오늘 하루를 들려주세요.
-          <br />
-          AI 로 감정을 분석해줄게요.
-          <br /> 최근 내 기분이 어땠는지 확인해보세요!
+          회원가입이 완료되었습니다.
+          <br /> 이제 오늘을 기록하고, <br />
+          ai가 분석해주는 감정을 확인해보세요!
         </div>
       </div>
 
       <div className="bottom">
-        <Button type="submit" label="다음" variant="BlackFull" size="medium" />
+        <Button type="submit" label="시작하기" variant="BlackFull" size="medium" />
       </div>
     </DonePage>
   );
--- src/pages/done/styles.js
+++ src/pages/done/styles.js
@@ -6,16 +6,22 @@ export const DonePage = styled.main`
   width: 100%;
 
   & > .top {
+    height: calc(100vh - 170px);
     display: flex;
     flex-direction: column;
     gap: 15px;
     padding: 9vh 30px;
     text-align: center;
+    align-items: center;
+    justify-content: center;
     & > img {
       height: 104px;
     }
+    & > .title {
+      ${fontGenerator('40px', 'Regular', '110%', '4%')}
+    }
     & > .comment {
-      ${fontGenerator('19px', 'Regular', '130%', '4%')}
+      ${fontGenerator('22px', 'Regular', '120%', '4%')}
     }
   }
 
