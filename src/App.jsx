9b9ca2a8a1c872b660eff9796fe547277307663c
Sung-young <smes289@gmail.com>
2024-07-30 03:44:10 +0900

    modification-diaryList

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,6 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Login, NickNameReset, NotFound, PasswordReset, SignUp, Splash } from './pages';
 import DiaryList from './pages/DiaryList/DiaryList';
 
-// 데이터를 DiaryList 페이지 구현때문에 임시로 만들었어요.
-const mocData = [
-  {
-    id: 1,
-    createdDate: new Date('2024-07-19').getTime(),
-    emotionId: 1,
-    content: '안녀하세요 오늘 너무 즐거웠어요호호 . . .',
-  },
-  {
-    id: 2,
-    createdDate: new Date('2024-07-18').getTime(),
-    emotionId: 2,
-    content: '2번 일기 내용',
-  },
-  {
-    id: 3,
-    createdDate: new Date('2024-07-28').getTime(),
-    emotionId: 3,
-    content: '3번 일기 내용',
-  },
-  {
-    id: 4,
-    createdDate: new Date('2024-07-29').getTime(),
-    emotionId: 1,
-    content: '4번 일기 내용',
-  },
-];
-
 function App() {
   return (
     <div className="App">
@@ -41,7 +13,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/password-reset" element={<PasswordReset />} />
           <Route path="/nickname-reset" element={<NickNameReset />} />
-          <Route path="/diaryList" element={<DiaryList data={mocData} />} />
+          <Route path="/diaryList" element={<DiaryList />} />
           {/* notFound : 일치하는 라우트 없는 경우 처리 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/pages/DiaryList/DiaryList.jsx
+++ src/pages/DiaryList/DiaryList.jsx
@@ -8,7 +8,36 @@ import emotionList from '../../util/constants';
 import DiaryItem from '../../components/DiaryItem/DiaryItem';
 import { Images } from '../../styles/images';
 
-function DiaryList({ data }) {
+const mocData = [
+  {
+    id: 1,
+    createdDate: new Date('2024-07-19').getTime(),
+    emotionId: 1,
+    content: '안녀하세요 오늘 너무 즐거웠어요호호 . . .',
+  },
+  {
+    id: 2,
+    createdDate: new Date('2024-07-18').getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+  {
+    id: 3,
+    createdDate: new Date('2024-07-28').getTime(),
+    emotionId: 3,
+    content: '3번 일기 내용',
+  },
+  {
+    id: 4,
+    createdDate: new Date('2024-07-29').getTime(),
+    emotionId: 1,
+    content: '4번 일기 내용',
+  },
+];
+
+function DiaryList() {
+  const data = mocData;
+
   const nav = useNavigate();
   const [selectedEmotionId, setSelectedEmotionId] = useState(1);
 
