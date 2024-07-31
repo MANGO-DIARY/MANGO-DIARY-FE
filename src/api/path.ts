02a34136c9e97a8ac8cfcccdb2173a455adf6865
minjeoong <orange_mj@naver.com>
2024-07-31 16:51:23 +0900

    ✨ Feat: api 경로 설정

---
+++ src/api/path.ts
@@ -0,0 +1,17 @@
+export const PATH_API = {
+  API_DOMAIN: 'http://43.200.195.86:8080/api/v1',
+  // auth
+  SIGN_IN: '/auth/sign-in',
+  SIGN_UP: '/auth/sign-up',
+  SIGN_OUT: '/auth/sign-out',
+  PASSWORD_RESET: '/auth/reset-pw',
+  // refresh token
+  TOKEN_REISSUE: '/auth/token-reissue',
+  SEND_EMAIL: '/auth/mail/send',
+  VERIFY_EMAIL: '/auth/mail/verify',
+};
+
+/**
+- /oauth/{provider}/sign-in-uri
+- /oauth/{provider}/sign-in
+ */
