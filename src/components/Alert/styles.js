65f43d93c91c56ac2e6a8609d2a47770c1d3c941
minjeoong <orange_mj@naver.com>
2024-07-31 17:58:46 +0900

    ✨ Feat: Alert 컴포넌트 init

---
+++ src/components/Alert/styles.js
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+import { fontGenerator } from '../../styles/styles.js';
+import { Colors } from '../../styles/colors.js';
+
+export const InputWrap = styled.main`
+  width: 100%;
+  transition: 1s;
+  & > .main {
+    height: 32px;
+    display: flex;
+    width: 100%;
+    gap: 10px;
+    padding: 6px;
+    border-bottom: 1px solid ${Colors.Gray03};
+    & > .SearchIcon {
+      width: 20px;
+      height: 20px;
+    }
+    & > input {
+      border: none;
+      background-color: unset;
+      font-family: 'Ownglyph_ryurue-Rg', sans-serif !important;
+      ${fontGenerator('19px', 'Regular', '100%', '4%')};
+      width: 100%;
+    }
+    & > input:focus {
+      outline: none;
+    }
+    & > .CloseIcon {
+      cursor: pointer;
+    }
+  }
+  & > .RHFHelperText {
+    display: block;
+    color: ${Colors.Error};
+  }
+`;
