a0944a698337ee0af455b60af84fa002ffe7dab4
haram8009 <haram8009@gmail.com>
2024-08-02 03:16:05 +0900

    🐛bug: 환경변수 에러 해결 #41

--- static.js
+++ static.js
@@ -1,3 +1,3 @@
-export const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
-export const ACCESS_TOKEN_KEY = process.env.REACT_APP_ACCESS_TOKEN_KEY;
-export const REFRESH_TOKEN_KEY = process.env.REACT_APP_REFRESH_TOKEN_KEY;
+export const API_DOMAIN = import.meta.env.VITE_APP_API_DOMAIN;
+export const ACCESS_TOKEN_KEY = import.meta.env.VITE_APP_ACCESS_TOKEN_KEY;
+export const REFRESH_TOKEN_KEY = import.meta.env.VITE_APP_REFRESH_TOKEN_KEY;
