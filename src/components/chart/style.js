e1eb5085563975d6b0d859c5eca7738df52bf961
haram8009 <haram8009@gmail.com>
2024-08-03 03:35:33 +0900

    ✨feat: chart 컴포넌트 스타일링

---
+++ src/components/chart/style.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import { fontGenerator } from '../../styles/styles.js';
+import { Colors } from '../../styles/colors.js';
+
+export const ChartContainer = styled.div`
+  // overflow-x, y축 선 삭제
+  .MuiChartsAxis-directionY,
+  .MuiChartsAxis-left,
+  .MuiChartsAxis-line,
+  .MuiChartsAxis-tick {
+    display: none;
+  }
+
+  // 차트 바(<rect>) 커스텀
+  [class*='MuiBarElement-series-auto-generated'] {
+    rx: 10; // 모서리 둥글게
+    width: 15px !important;
+    x: 9px; // 바 가운데로 오도록 수정
+  }
+`;
