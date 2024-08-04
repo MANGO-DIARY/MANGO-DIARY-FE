57b718869c138b4aec59222e123b4c8836a6dcfd
haram8009 <haram8009@gmail.com>
2024-08-05 04:12:45 +0900

    ✨feat: chart 페이지 default 퍼블리싱

--- src/pages/chart/chart.jsx
+++ src/pages/chart/chart.jsx
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Stack, Typography } from '@mui/material';
 import Chart from '../../components/chart/chart';
 import { Header, NavBar } from '../../components';
 import { Images } from '../../styles/images';
+import { Colors } from '../../styles/colors';
+
+const example = {
+  yearMonth: {
+    year: 0,
+    month: 'JANUARY',
+    monthValue: 0,
+    leapYear: true,
+  },
+  emotionCounts: {
+    기쁨: 2,
+    신남: 5,
+    행복: 2,
+    평온: 3,
+    슬픔: 1,
+    분노: 2,
+    불안: 1,
+    우울: 0,
+  },
+  monthlyComment: 'monthlyComment',
+  statisticsComment: 'statisticsComment',
+};
 
 export default function ChartPage() {
   const navigate = useNavigate();
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    setChartData(example);
+  }, []);
+
   return (
     <>
       <Header title="월간통계" iconSrc={Images.left} onClick={() => navigate(-1)} />
-      <Chart />
+      <Stack sx={{ paddingX: '30px' }}>
+        <Typography variant="h3">
+          망고럭키님의 <br />
+          7월 리포트
+        </Typography>
+        <Typography variant="body" color={Colors.Gray06}>
+          yyyy년 MM월 DD일 기준
+        </Typography>
+
+        <Typography sx={{ mt: 5 }} textAlign="center" color={Colors.Gray02}>
+          {chartData.statisticsComment}
+        </Typography>
+      </Stack>
+      <Chart chartData={chartData.emotionCounts} />
+      <Stack sx={{ paddingX: '30px', paddingY: 5 }}>
+        <Typography textAlign="center" color={Colors.Gray05}>
+          이번달에 아직 일기를 작성하지 않았어요. <br />
+          일기를 작성하고 ai의 코멘트를 받아보세요!
+        </Typography>
+      </Stack>
       <NavBar />
     </>
   );
