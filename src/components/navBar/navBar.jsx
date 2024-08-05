0d92eaf1c0e542553d2b4a4f820079ad2bfd2e82
haram8009 <haram8009@gmail.com>
2024-08-06 02:13:36 +0900

    ✨feat: navBar navigate 활성화

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { Images } from '../../styles/images';
 import { Colors } from '../../styles/colors';
+import { PATH } from '../../route/path.js';
 
 const BottomNav = styled(BottomNavigation)`
   background-color: transparent;
@@ -27,11 +29,41 @@ function NavBar() {
         //   setValue(newValue);
         // }}
       >
-        <BottomNavAction label="홈" icon={<img src={Images.home} alt="plus-icon" />} />
-        <BottomNavAction label="일기내역" icon={<img src={Images.calendar} alt="plus-icon" />} />
-        <BottomNavAction label="일기작성" icon={<img src={Images.plus} alt="plus-icon" />} />
-        <BottomNavAction label="통계" icon={<img src={Images.barchart} alt="plus-icon" />} />
-        <BottomNavAction label="설정" icon={<img src={Images.settings} alt="plus-icon" />} />
+        <BottomNavAction
+          label="홈"
+          icon={<img src={Images.home} alt="plus-icon" />}
+          onClick={() => {
+            navigate(PATH.HOME);
+          }}
+        />
+        <BottomNavAction
+          label="일기내역"
+          icon={<img src={Images.calendar} alt="plus-icon" />}
+          onClick={() => {
+            navigate(PATH.CALENDAR);
+          }}
+        />
+        <BottomNavAction
+          label="일기작성"
+          icon={<img src={Images.plus} alt="plus-icon" />}
+          onClick={() => {
+            navigate(PATH.HOME);
+          }}
+        />
+        <BottomNavAction
+          label="통계"
+          icon={<img src={Images.barchart} alt="plus-icon" />}
+          onClick={() => {
+            navigate(PATH.CHART);
+          }}
+        />
+        <BottomNavAction
+          label="설정"
+          icon={<img src={Images.settings} alt="plus-icon" />}
+          onClick={() => {
+            navigate(PATH.HOME);
+          }}
+        />
       </BottomNav>
     </Box>
   );
