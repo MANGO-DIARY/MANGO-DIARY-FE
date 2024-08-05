424823b6dee07d2451a16059d5fcd0069cf4bd23
minjeoong <orange_mj@naver.com>
2024-08-06 01:36:40 +0900

    🐛 Fix: 이메일 전송 api 연동

--- src/api/path.js
+++ src/api/path.js
@@ -1,13 +1,13 @@
 export const PATH_API = {
-  API_DOMAIN: 'http://43.200.195.86:8080',
+  API_DOMAIN: 'http://43.200.195.86:8080/api/v1',
   API_DOMAIN_LOCAL: 'https://mangolion-server.site',
   REDIRECT_URL: 'http://localhost:5173/home',
   // auth
   SIGN_IN: '/auth/sign-in',
   SIGN_UP: '/auth/sign-up',
   SIGN_OUT: '/auth/sign-out',
-  KAKAO_LOGIN_URL: '/api/v1/oauth/kakao/sign-in-url',
-  KAKAO_LOGIN: '/api/v1/oauth/kakao/sign-in',
+  KAKAO_LOGIN_URL: '/oauth/kakao/sign-in-url',
+  KAKAO_LOGIN: '/oauth/kakao/sign-in',
   PASSWORD_RESET: '/auth/reset-pw',
   // refresh token
   TOKEN_REISSUE: '/auth/token-reissue',
--- src/pages/passwordReset/passwordReset.jsx
+++ src/pages/passwordReset/passwordReset.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from 'react-router-dom';
+import { Alert } from '@mui/material';
 import { PasswordResetWrap } from './styles';
 import InputForm from '../../components/InputForm/inputForm';
 import FormProvider from '../../components/formProvider/FormProvider';
@@ -10,6 +11,10 @@ import FormProvider from '../../components/formProvider/FormProvider';
 import { Images } from '../../styles/images';
 import Header from '../../components/header/Header.jsx';
 import Button from '../../components/button/button.jsx';
+import { useSendEmail } from '../../api/queries/auth/send-email.js';
+import { useVerifyEmail } from '../../api/queries/auth/verify-email.js';
+import { usePasswordReset } from '../../api/queries/auth/password-reset.js';
+import useEmailAuthStore from '../../store/auth/emailAuthStore.js';
 
 const signUpSchema = Yup.object().shape({
   userEmail: Yup.string()
@@ -32,9 +37,12 @@ const defaultValues = {
 function PasswordReset() {
   const navigate = useNavigate();
   const [showVerificationInput, setShowVerificationInput] = useState(false);
-
-  // const resetPwMutation = usePasswordInit();
-  // const sendEmailMutation = useSendEmail();
+  const { endAt, isAuthenticated, setEndAt, setIsAuthenticated } = useEmailAuthStore((state) => state);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const { mutate: passwordResetMutate, isPending: passwordResetPending } = usePasswordReset();
+  const { mutate: emailMutate, isPending: emailPending } = useSendEmail();
+  const { mutate: verifyMutate, isPending: verifyPending } = useVerifyEmail();
 
   const methods = useForm({
     defaultValues,
@@ -45,34 +53,70 @@ function PasswordReset() {
   const {
     handleSubmit,
     formState: { isValid, errors },
+    watch,
   } = methods;
 
   const onSubmit = (data) => {
-    // const { password } = data;
-    //
-    // resetPwMutation.mutate({
-    //   userEmail,
-    //   password: sha256(password),
-    // });
+    const { userEmail, password } = data;
+    passwordResetMutate(
+      {
+        userEmail: userEmail,
+        password: password,
+      },
+      {
+        onSuccess: () => {},
+        onError: () => {},
+      }
+    );
+  };
+  const sendEmail = () => {
+    setShowVerificationInput(true);
+    if (endAt && new Date() < new Date(endAt)) {
+      return;
+    }
+    emailMutate(
+      {
+        userEmail: watch('userEmail'),
+        purpose: 'RESET_PASSWORD',
+      },
+      {
+        onSuccess: (data) => {
+          setEndAt(Date.now() + 1000 * 60 * 3);
+          setSuccessMessage(data);
+          setEmailDisabled(true);
+        },
+        onError: (error) => {
+          if (error.response?.status === 409) {
+            // DO
+          } else {
+            setErrorMessage(error?.message);
+          }
+        },
+      }
+    );
   };
 
-  const sendEmail = () => {
-    // if (endAt && new Date() < new Date(endAt)) {
-    //   setIsOtpModalOpen(true);
-    //   return;
-    // }
-    //
-    // sendEmailMutation.mutate(
-    //   {
-    //     userEmail: watch('userEmail'),
-    //     purpose: EmailVerifyPurpose.RESET_PASSWORD,
-    //   },
-    //   {
-    //     onSuccess: () => {//
-    //       setEndAt(Date.now() + 1000 * 60 * 3);
-    //     },
-    //   }
-    // );
+  const verifyEmail = () => {
+    verifyMutate(
+      {
+        userEmail: watch('userEmail'),
+        code: watch('verificationCode'),
+      },
+      {
+        onSuccess: () => {
+          setEndAt(null);
+          setIsAuthenticated(true);
+          setSuccessMessage('이메일 인증이 완료되었습니다.');
+          setErrorMessage('');
+          setVerificationDisabled(true);
+        },
+        onError: (error) => {
+          console.log(error.message);
+          setErrorMessage('이메일 인증을 실패하였습니다. 새로고침으로 다시 진행해주세요.');
+          setSuccessMessage('');
+        },
+      }
+    );
   };
 
   const handleSendVerification = () => {
@@ -95,9 +139,8 @@ function PasswordReset() {
             purpose={{
               isUsed: true,
               label: '인증번호 발송',
-              onClick: () => {
-                handleSendVerification();
-              },
+              isPending: emailPending,
+              onClick: () => sendEmail(),
             }}
           />
           {showVerificationInput && (
@@ -108,7 +151,8 @@ function PasswordReset() {
               purpose={{
                 isUsed: true,
                 label: '확인',
-                onClick: () => console.log('확인 버튼 클릭'),
+                isPending: verifyPending,
+                onClick: () => verifyEmail(),
               }}
             />
           )}
@@ -120,6 +164,28 @@ function PasswordReset() {
           <Button type="submit" label="비밀번호 재설정하기" variant="BlackFull" size="medium" disabled={!isValid} />
         </div>
       </FormProvider>
+      {successMessage && (
+        <Alert
+          severity="success"
+          onClose={() => {
+            setSuccessMessage('');
+          }}
+          sx={{ margin: '0 30px' }}
+        >
+          {successMessage}
+        </Alert>
+      )}
+      {errorMessage && (
+        <Alert
+          severity="error"
+          onClose={() => {
+            setErrorMessage('');
+          }}
+          sx={{ margin: '0 30px' }}
+        >
+          {errorMessage}
+        </Alert>
+      )}
     </PasswordResetWrap>
   );
 }
