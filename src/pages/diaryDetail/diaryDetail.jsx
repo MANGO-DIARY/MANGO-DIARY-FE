7dbc67472808b6f90c4d01caf732c967a79870b4
haram8009 <haram8009@gmail.com>
2024-08-06 15:28:01 +0900

    ✨feat: 일기상세 api 연동

--- src/pages/diaryDetail/diaryDetail.jsx
+++ src/pages/diaryDetail/diaryDetail.jsx
@@ -6,22 +6,13 @@ import { Images } from '../../styles/images';
 import getHeaderDate from '../../util/getHeaderDate';
 import { DiaryDetailWrap } from './styles';
 import { AiComment, NavBar } from '../../components';
-
-const mockupData = {
-  id: 1,
-  content:
-    '오늘은 가족과 함께 놀이공원에 갔다. 아침 일찍 출발해서 놀이공원에 도착하니 벌써 많은 사람들이 있었다. 먼저 롤러코스터를 탔다. 처음에는 무서웠지만, 타고 나니 정말 신났다. 그 다음에는 관람차를 타고 높은 곳에서 놀이공원을 내려다봤다. 풍경이 정말 아름다웠다.',
-  date: '2024-08-02',
-  emotion: '기쁨',
-  aiComment: '오늘 하루 정말 즐거운 시간을 보낸 것 같아서 기뻐요. 이런 소중한 추억들이 앞으로도 많은 힘이 될 거예요!',
-};
+import { useDiaryDetail } from '../../api/queries/diary/diary-detail';
 
 function DiaryDetail() {
   const { diaryId } = useParams();
   const navigate = useNavigate();
   const [date, setDate] = useState('2024.08.06.');
-  const [diaryData, setDiaryData] = useState(mockupData);
-  const [isLoading, setIsLoading] = useState(true);
+  const { data: diaryData, isLoading: isDiaryLoading, refetch } = useDiaryDetail({ diaryId });
 
   function handleDeleteClick() {
     const isDelete = window.confirm('일기를 삭제하시겠습니까?');
@@ -32,15 +23,25 @@ function DiaryDetail() {
   }
 
   useEffect(() => {
-    console.log(diaryId);
-    setDiaryData(mockupData);
-    setDate(getHeaderDate(mockupData.date));
-    setIsLoading(true);
-  }, [diaryId]);
+    if (diaryId) {
+      console.log(diaryId);
+      refetch();
+    }
+  }, [refetch, diaryId]);
+
+  useEffect(() => {
+    if (diaryData) {
+      setDate(getHeaderDate(diaryData?.date));
+    }
+  }, [diaryData]);
+
+  if (isDiaryLoading) {
+    return <div>로딩중...</div>;
+  }
 
   return (
-    isLoading && (
-      <DiaryDetailWrap emotion={diaryData.emotion} className="use-navbar">
+    diaryData && (
+      <DiaryDetailWrap emotion={diaryData?.emotion} className="use-navbar">
         <Header
           title={date}
           iconSrc={Images.left}
@@ -54,11 +55,11 @@ function DiaryDetail() {
         <Stack>
           <div className="top">
             <img alt="emotion" />
-            <p id="emotion-name">{diaryData.emotion}</p>
+            <p id="emotion-name">{diaryData?.emotion}</p>
           </div>
-          <article>{diaryData.content}</article>
+          <article>{diaryData?.content}</article>
           <div className="ai-wrap">
-            <AiComment aiComment={diaryData.aiComment} />
+            <AiComment aiComment={diaryData?.aiComment} />
           </div>
         </Stack>
         <NavBar />
