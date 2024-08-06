7b9841c7bce199b5a545f2c7b10a5086bcfe687c
haram8009 <haram8009@gmail.com>
2024-08-06 21:17:13 +0900

    �fix: main 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,37 +1,20 @@
 /** @jsxImportSource @emotion/react */
-import React, { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import DiaryItem from '../../components/DiaryItem/DiaryItem';
 import { Images } from '../../styles/images';
 import NavBar from '../../components/navBar/navBar';
 import { MainContainer, MainTop, Comment, MainMiddle, FrameHeader, HeaderButton, MainBottom, Rank, EmotionRank, First, Second, Third, Phase } from './Main.styles';
 import { useMain } from '../../api/queries/main/main';
 import { useUserInfo } from '../../api/queries/user/useUserInfo';
-import { useKakaoLogin } from '../../api/queries/auth/kakao-login.js';
 import getEmotionImage from '../../util/get-emotion-img';
-import { PATH_API } from '../../api/path.js';
-import Spiner from '../../components/Spiner/Spiner.jsx';
+import { PATH } from '../../route/path';
+import Spiner from '../../components/Spiner/Spiner';
 
 function Main() {
   const nav = useNavigate(); // 네비게이션 훅
-  const { data: CommentData } = useMain();
   const { data: mainData, isLoading: isMainLoading } = useMain(); // 데이터, 로딩 상태, 에러 상태
   const { data: userInfo, isLoading: isUserLoading } = useUserInfo(); // 데이터, 로딩 상태, 에러 상태
-
-  // 현재 URL 정보를 가져오는 훅
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const code = queryParams.get('code');
-  const { mutate, isLoading: KakaoLoading } = useKakaoLogin(code);
-
-  useEffect(() => {
-    if (code) {
-      mutate({
-        code,
-        redirectUri: PATH_API.REDIRECT_URL,
-      });
-    }
-  }, [code, mutate]);
 
   // 로딩 중 상태 처리
   if (isMainLoading || isUserLoading) return <Spiner />;
@@ -45,17 +28,24 @@ function Main() {
         <div>
           <img src={Images.joy} alt="icon" />
         </div>
-        <Comment>{CommentData?.todayComment}</Comment>
+        {hasData ? <Comment>{mainData.todayComment}</Comment> : '오늘의 코멘트가 없어요'}
       </MainTop>
       <MainMiddle>
         <FrameHeader>
           {userInfo?.userName}님의 최근 일기
-          <HeaderButton onClick={() => nav('/diary-list')}>
+          <HeaderButton onClick={() => nav(PATH.DIARYLIST)}>
             <img src={Images.right} alt="icon" />
           </HeaderButton>
         </FrameHeader>
         {hasData && mainData.todayDiary ? (
-          <DiaryItem onClick={nav('/diary/detail/:diaryId')} emotion={mainData?.todayDiary.emotion} date={mainData.todayDiary.date} content={mainData.todayDiary.content} />
+          <DiaryItem
+            onClick={() => {
+              nav(`${PATH.DIARYDETAIL_ENDPOINT}${mainData.todayDiary.id}`);
+            }}
+            emotion={mainData.todayDiary.emotion}
+            date={mainData.todayDiary.date}
+            content={mainData.todayDiary.content}
+          />
         ) : (
           <DiaryItem content="작성하신 일기가 없습니다. 일기를 작성해 주세요" formattedDate={null} emotion="none" />
         )}
@@ -70,25 +60,37 @@ function Main() {
         <Rank>
           <EmotionRank>
             {hasData && mainData.topThreeEmotionThisMonth[0] && (
-              <First onClick={nav('/chart')}>
+              <First
+                onClick={() => {
+                  nav(PATH.CHART);
+                }}
+              >
                 <img src={getEmotionImage(mainData.topThreeEmotionThisMonth[0]?.emotion)} alt="img" />
               </First>
             )}
             {hasData && mainData.topThreeEmotionThisMonth[1] && (
-              <Second onClick={nav('/chart')}>
+              <Second
+                onClick={() => {
+                  nav(PATH.CHART);
+                }}
+              >
                 <img src={getEmotionImage(mainData.topThreeEmotionThisMonth[1]?.emotion)} alt="img" />
               </Second>
             )}
             {hasData && mainData.topThreeEmotionThisMonth[2] && (
-              <Third onClick={nav('/chart')}>
+              <Third
+                onClick={() => {
+                  nav(PATH.CHART);
+                }}
+              >
                 <img src={getEmotionImage(mainData.topThreeEmotionThisMonth[2]?.emotion)} alt="img" />
               </Third>
             )}
             {!hasData && (
               <>
-                <First></First>
-                <Second></Second>
-                <Third></Third>
+                <First />
+                <Second />
+                <Third />
               </>
             )}
           </EmotionRank>
@@ -97,7 +99,6 @@ function Main() {
           </Phase>
         </Rank>
       </MainBottom>
-
       <NavBar />
     </MainContainer>
   );
